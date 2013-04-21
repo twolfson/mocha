21194ab558325a51817ef0e655195bb202d31c66
Todd Wolfson <todd@twolfson.com>
2013-04-20 21:49:56 -0700

    Cross-contamination does not seem to exist here o_O

---
+++ test/acceptance/batch.isolation.js
@@ -0,0 +1,22 @@
+console.log('aaa');
+describe('One context', function () {
+  before(function () {
+    this.context = '1';
+  });
+
+  it('is isolated from other contexts', function () {
+    console.log(this.context);
+    this.context.should.equal('1');
+  });
+});
+
+describe('Another context', function () {
+  before(function () {
+    this.context = '2';
+  });
+
+  it('is isolated from the first context', function () {
+    console.log(this.context);
+    this.context.should.equal('2');
+  });
+});
