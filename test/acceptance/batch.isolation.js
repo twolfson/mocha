8e11b56c94382dc7e6ec19a824f225ae734b362d
Todd Wolfson <todd@twolfson.com>
2013-04-20 21:51:24 -0700

    I am at a loss =(

--- test/acceptance/batch.isolation.js
+++ test/acceptance/batch.isolation.js
@@ -4,10 +4,10 @@ describe('One context', function () {
     this.context = '1';
   });
 
-  it('is isolated from other contexts', function () {
-    console.log(this.context);
-    this.context.should.equal('1');
-  });
+  // it('is isolated from other contexts', function () {
+  //   console.log(this.context);
+  //   this.context.should.equal('1');
+  // });
 
   describe('nested', function () {
     it('is isolated from other contexts', function () {
@@ -22,10 +22,10 @@ describe('Another context', function () {
     this.context = '2';
   });
 
-  it('is isolated from the first context', function () {
-    console.log(this.context);
-    this.context.should.equal('2');
-  });
+  // it('is isolated from the first context', function () {
+  //   console.log(this.context);
+  //   this.context.should.equal('2');
+  // });
 
   describe('nested', function () {
     it('is isolated from the first context', function () {
