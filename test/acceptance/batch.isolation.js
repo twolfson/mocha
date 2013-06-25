2e162801f571c5e9d3c2b06f6bc57e933af26d45
Todd Wolfson <todd@twolfson.com>
2013-06-25 01:53:35 -0700

    Just tried with `mocha test/acceptance/batch.isolation.js test/acceptance/batch.isolation2.js` and still contaminates

---
+++ test/acceptance/batch.isolation.js
@@ -0,0 +1,50 @@
+var assert = require('assert');
+describe('One context', function () {
+  before(function () {
+    this.context = this.context || [];
+    this.context.push(1);
+  });
+
+  it('is isolated from other contexts', function () {
+    assert.deepEqual(this.context, [1]);
+  });
+
+  describe('nested', function () {
+    before(function () {
+      this.context.push(2);
+    });
+
+    it('is isolated from other contexts', function () {
+      assert.deepEqual(this.context, [1, 2]);
+    });
+  });
+
+  it('is isolated from other contexts', function () {
+    assert.deepEqual(this.context, [1]);
+  });
+});
+
+// describe('Another context', function () {
+//   before(function () {
+//     this.context = this.context || [];
+//     this.context.push(3);
+//   });
+
+//   it('is isolated from the first context', function () {
+//     this.context.should.equal([3]);
+//   });
+
+//   describe('nested', function () {
+//     before(function () {
+//       this.context.push(4);
+//     });
+
+//     it('is isolated from the first context', function () {
+//       this.context.should.equal([3, 4]);
+//     });
+//   });
+
+//   it('is isolated from the first context', function () {
+//     this.context.should.equal([3]);
+//   });
+// });
---
+++ test/acceptance/batch.isolation2.js
@@ -0,0 +1,51 @@
+var assert = require('assert');
+// describe('One context', function () {
+//   before(function () {
+//     this.context = this.context || [];
+//     this.context.push(1);
+//   });
+
+//   it('is isolated from other contexts', function () {
+//     this.context.should.equal([1]);
+//   });
+
+//   describe('nested', function () {
+//     before(function () {
+//       this.context.push(2);
+//     });
+
+//     it('is isolated from other contexts', function () {
+//       this.context.should.equal([1, 2]);
+//     });
+//   });
+
+//   it('is isolated from other contexts', function () {
+//     this.context.should.equal([1]);
+//   });
+// });
+
+describe('Another context', function () {
+  before(function () {
+    console.log(this.context);
+    this.context = this.context || [];
+    this.context.push(3);
+  });
+
+  it('is isolated from the first context', function () {
+    assert.deepEqual(this.context, [3]);
+  });
+
+  describe('nested', function () {
+    before(function () {
+      this.context.push(4);
+    });
+
+    it('is isolated from the first context', function () {
+      assert.deepEqual(this.context, [3, 4]);
+    });
+  });
+
+  it('is isolated from the first context', function () {
+    assert.deepEqual(this.context, [3]);
+  });
+});
