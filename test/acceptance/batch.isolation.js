83e5cd8b2479102a9bad3a603bf353fecb8a7f94
Todd Wolfson <todd@twolfson.com>
2013-06-27 23:50:20 -0700

    Successfully reproducing issue seen in doubleshot. afterEach is being run after *every* context

--- test/acceptance/batch.isolation.js
+++ test/acceptance/batch.isolation.js
@@ -1,51 +1,64 @@
 var assert = require('assert');
 describe('One context', function () {
   before(function () {
+    console.log('before1');
     this.context = this.context || [];
     this.context.push(1);
   });
 
   it('is isolated from other contexts', function () {
+    console.log('it1');
     assert.deepEqual(this.context, [1]);
   });
 
   describe('nested', function () {
     before(function () {
+      console.log('before2');
       this.context.push(2);
     });
 
     it('is isolated from other contexts', function () {
+      console.log('it2');
       assert.deepEqual(this.context, [1, 2]);
     });
   });
 
   it('is isolated from other contexts', function () {
+    console.log('it1b');
     assert.deepEqual(this.context, [1]);
   });
 });
 
-describe('Another context', function () {
-  before(function () {
-    console.log(this.context);
-    this.context = this.context || [];
-    this.context.push(3);
-  });
+// describe('Another context', function () {
+//   before(function () {
+//     console.log(this.context);
+//     this.context = this.context || [];
+//     this.context.push(3);
+//   });
 
-  it('is isolated from the first context', function () {
-    assert.deepEqual(this.context, [3]);
-  });
+//   it('is isolated from the first context', function () {
+//     assert.deepEqual(this.context, [3]);
+//   });
 
-  describe('nested', function () {
-    before(function () {
-      this.context.push(4);
-    });
+//   describe('nested', function () {
+//     before(function () {
+//       this.context.push(4);
+//     });
 
-    it('is isolated from the first context', function () {
-      assert.deepEqual(this.context, [3, 4]);
-    });
-  });
+//     it('is isolated from the first context', function () {
+//       assert.deepEqual(this.context, [3, 4]);
+//     });
+//   });
 
-  it('is isolated from the first context', function () {
-    assert.deepEqual(this.context, [3]);
-  });
+//   it('is isolated from the first context', function () {
+//     assert.deepEqual(this.context, [3]);
+//   });
+// });
+
+afterEach(function () {
+  console.log('after');
+  // var key;
+  // for (key in this) {
+  //   delete this[key];
+  // }
 });
